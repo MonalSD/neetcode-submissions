34405f70db4c035a78c7541f0b95c263a55c9367
Monal Singh <77961623+MonalSD@users.noreply.github.com>
2026-05-24 17:36:56 +0530

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> digitToChar = {
+            "", "", "abc", "def", "ghi", "jkl",
+            "mno", "pqrs", "tuv", "wxyz"
+        };
+    vector<string>res;
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty())
+            return res;
+        recur(0,"",digits);
+        return res;
+    }
+    void recur(int i,string curr,string & digits){
+        if(curr.size()==digits.size()){
+            res.push_back(curr);
+            return;
+        }
+        string ch = digitToChar[digits[i]-'0'];
+        for(char c: ch)
+            recur(i+1,curr+c,digits);
+    }
+};
