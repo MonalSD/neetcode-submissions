353856b92b98f12bfeeb6ac61aa92aa1c16a7b4e
Monal Singh <77961623+MonalSD@users.noreply.github.com>
2026-05-31 13:11:47 +0530

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.cpp
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(node==NULL)
+            return node;
+        unordered_map<Node*, vector<Node*>> mp;
+        vector<bool> visit(101, false);
+        trav(node,visit,mp);
+        unordered_map<Node*, Node*> cloneMap;
+        for (auto it : mp) {
+            Node* original = it.first;
+            cloneMap[original] = new Node(original->val);
+        }
+        for (auto it : mp) {
+            Node* original = it.first;
+            vector<Node*> neighbors = it.second;
+
+            for (auto neighbor : neighbors) {
+                cloneMap[original]->neighbors.push_back(cloneMap[neighbor]);
+            }
+        }
+        return cloneMap[node];
+    }
+    void trav(Node* node,vector<bool>& visit,unordered_map<Node*,vector<Node*>>& mp){
+        if(node==NULL)
+            return;
+        if(visit[node->val]==true)
+            return;
+        visit[node->val]=true;
+        mp[node] = node->neighbors;
+        for(auto i:node->neighbors){
+            trav(i,visit,mp);
+        }
+    }
+};
